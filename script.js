439ccaa8dc75a631936975a76b5acc7a8479e383
Vince Chow <vchow.829@gmail.com>
2021-04-02 23:17:15 -0700

    2nd upload. Added a new chunk of code at bottom of js file for now. The starting pieces that was default in the js file doesn't seem to provide something that makes sense to start with.

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(characters);
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-
-
-  passwordText.value = password;
+  
+passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var generatePassword = function() {
+  
+}
+
+// Vince starting over for now
+
+//generate random password
+function generate() {
+
+  var length = document.getElementById("generate").value;
+  
+  //possible characters for password
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+  var password = "";
+
+  //for loop to choose characters
+  for(var i =0; i <= length; i++); {
+    password = password + characters.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+  }
+}
 
