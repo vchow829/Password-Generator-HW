a6d0d6296d56855af29a6b2ab9045c299c1fdc8a
Vince Chow <vchow.829@gmail.com>
2021-04-03 20:17:54 -0700

    4th upload. Got more aspects to work although is not near complete. Uploading and submitting what I have currently prior to deadline.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password-Generator-HW
+
+This assignment is very tough and was frustrating in the beginning. It still is frustrating but I made some progress. 
+
+I know this is wrong because it doesn't function correctly but I think I have some aspects necessary such as console.log, math random, window prompts and confirm, etc.
+
+I am currently uploading and submitting what I have before the deadline and will continue to work on this assignment.
--- script.js
+++ script.js
@@ -2,50 +2,71 @@
 var generateBtn = document.querySelector("#generate");
 var generatePassword = function() {
   var length = prompt("What length does your password need to be?");
-  console.log(length);
+  console.log("Password length:", length);
+
+// gives warning if prompt is left blank
+  if (!length || !length.includes(length)) {
+    console.warn("Must be between 8 and 128 characters", length);
+    return;}
 
   var uCase = confirm("Do you want upper case letters?");
-  console.log(uCase);
+  console.log("Wants upper case?", uCase);
+  if (uCase === true); {
+    uCase=String.fromCharCode(Math.floor(Math.random()*26)+65);
+    console.log(uCase);
+  }
 
   var lCase = confirm("Do you want lower case letters?");
-  console.log(lCase);
+  console.log("Wants lower case?", lCase);
+  if (lCase === true); {
+    lCase=String.fromCharCode(Math.floor(Math.random()*26)+97);
+    console.log(lCase);
+  }
 
   var spCharacter = confirm("Do you want special characters?");
-  console.log(spCharacter);
+  console.log("Wants special characters?", spCharacter);
+  if (spCharacter === true); {
+    spCharacter=String.fromCharCode(Math.floor(Math.random()*33)+43);
+    console.log(spCharacter);
+  }
+
 
   var number = confirm("Do you want numbers?");
-  console.log(number);
+  console.log("Wants numbers?", number);
+  if (number === true) {
+    number = Math.floor(Math.random() * 10);
+    console.log(number);
+  }
 }
-generateBtn.addEventListener("click", generatePassword);}
+
+generateBtn.addEventListener("click", generatePassword);
 
 // Write password to the #password input
 function writePassword() {
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  generatePassword() 
+    if (uCase === true); {
+      uCase=String.fromCharCode(Math.floor(Math.random()*26)+65);
+      console.log(uCase);
+    }
+    
+    if (lCase === true); {
+      lCase=String.fromCharCode(Math.floor(Math.random()*26)+97);
+      console.log(lCase);
+    }
+    if (spCharacter === true); {
+      spCharacter=String.fromCharCode(Math.floor(Math.random()*33)+43);
+      console.log(spCharacter);
+    }
+
+     if (number === true) {
+    number = Math.floor(Math.random() * 10);
+    console.log(number);
+     }
   
+
 passwordText.value = password;
 
-}
-
-
-
-// Vince starting over for now
-
-//generate random password
-function generate() {
-
-  var length = document.getElementById("generate").value;
-  
-  //possible characters for password
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-  var password = "";
-
-  //for loop to choose characters
-  for(var i = 0; i <= length; i++); {
-    password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
-  }
-
-  //password to show in display box
-  document.getElementById("password").characters = password;
 }
